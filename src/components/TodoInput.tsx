563bc15f1f4fcbf48b0ff91a835294f537b5169d
GUSTAVO ROCHA TAMBELINI <rochatambelini@gmail.com.br>
2021-05-20 21:33:58 -0300

    Project Working

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -8,10 +8,11 @@ interface TodoInputProps {
 }
 
 export function TodoInput({ addTask }: TodoInputProps) {
-  // const [task, setTask] = useState('');
+   const [task, setTask] = useState('');
 
   function handleAddNewTask() {
-    //TODO - Call addTask and clean input value 
+    //TODO - Call addTask and clean input value
+    setTask('');
   }
 
   return (
@@ -20,13 +21,21 @@ export function TodoInput({ addTask }: TodoInputProps) {
         style={styles.input} 
         placeholder="Adicionar novo todo..."
         returnKeyType="send"
-        //TODO - use value, onChangeText and onSubmitEditing props
+        onChangeText={setTask}
+        value={task}
+        onSubmitEditing ={()=>{
+          addTask(task);
+          handleAddNewTask();
+        }}
       />
       <TouchableOpacity
         testID="add-new-task-button"
         activeOpacity={0.7}
         style={styles.addButton}
-        //TODO - onPress prop
+        onPress={()=>{
+          addTask(task);
+          handleAddNewTask();
+        }}
       >
         <Image source={checkIcon} />
       </TouchableOpacity>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,18 +11,41 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task if it's not empty
+    if(newTaskTitle.trim() != ''){    
+      const dataTask = {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false
+      }
+      setTasks(oldTask => [...oldTask, dataTask]);
+    }
+    
   }
 
   function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+    //TODO - mark task as done if exists   
+    let data : Task[];
+    data = tasks;
+
+    for (var i = 0; i < data.length ; i++) {
+        if(data[i].id == id){
+          data[i].done = !data[i].done;
+          break;
+        }   
+    }   
+
+    setTasks([...data]);
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    setTasks(oldTask => oldTask.filter(
+      task => task.id != id
+    ));
   }
 
   return (
